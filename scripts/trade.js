3a3608ed612ce75ce0f41d3128451122b858c4bc
KC <kc@KCdeMacBook-Pro.local>
2023-11-17 13:57:47 -0500

    chore: added console log in trade.js

--- scripts/trade.js
+++ scripts/trade.js
@@ -2,14 +2,18 @@ const hre = require("hardhat");
 const fs = require("fs");
 require("dotenv").config();
 
-let config,arb,owner,inTrade,balances;
+let config, arb, owner, inTrade, balances;
 const network = hre.network.name;
 if (network === 'aurora') config = require('./../config/aurora.json');
 if (network === 'fantom') config = require('./../config/fantom.json');
 
 console.log(`Loaded ${config.routes.length} routes`);
+console.log(`Loaded ${config.baseAssets.length} baseAssets`);
+console.log(`Loaded ${config.tokens.length} tokens`);
+console.log(`Loaded ${config.routers.length} routers`);
 
 const main = async () => {
+  console.log("start setup.... block at", await hre.ethers.provider.getBlockNumber())
   await setup();
   // Scale when using own node
   //[0,0,0,0,0,0,0,0,0].forEach(async (v,i) => {
@@ -21,10 +25,10 @@ const main = async () => {
 
 const searchForRoutes = () => {
   const targetRoute = {};
-  targetRoute.router1 = config.routers[Math.floor(Math.random()*config.routers.length)].address;
-  targetRoute.router2 = config.routers[Math.floor(Math.random()*config.routers.length)].address;
-  targetRoute.token1 = config.baseAssets[Math.floor(Math.random()*config.baseAssets.length)].address;
-  targetRoute.token2 = config.tokens[Math.floor(Math.random()*config.tokens.length)].address;
+  targetRoute.router1 = config.routers[Math.floor(Math.random() * config.routers.length)].address;
+  targetRoute.router2 = config.routers[Math.floor(Math.random() * config.routers.length)].address;
+  targetRoute.token1 = config.baseAssets[Math.floor(Math.random() * config.baseAssets.length)].address;
+  targetRoute.token2 = config.tokens[Math.floor(Math.random() * config.tokens.length)].address;
   return targetRoute;
 }
 
@@ -49,29 +53,36 @@ const lookForDualTrade = async () => {
     targetRoute = searchForRoutes();
   }
   try {
-    let tradeSize = balances[targetRoute.token1].balance;
+    let tradeSize = balances[targetRoute.token1].balance.div(2);
+    console.log('stare calling at', new Date(), { tradeSize, targetRoute });
     const amtBack = await arb.estimateDualDexTrade(targetRoute.router1, targetRoute.router2, targetRoute.token1, targetRoute.token2, tradeSize);
-    const multiplier = ethers.BigNumber.from(config.minBasisPointsPerTrade+10000);
+    const multiplier = hre.ethers.BigNumber.from(config.minBasisPointsPerTrade + 10000);
     const sizeMultiplied = tradeSize.mul(multiplier);
-    const divider = ethers.BigNumber.from(10000);
+    const divider = hre.ethers.BigNumber.from(10000);
     const profitTarget = sizeMultiplied.div(divider);
+    console.log({
+      amtBack,
+      sizeMultiplied,
+      profitTarget,
+    })
     if (!config.routes.length > 0) {
-      fs.appendFile(`./data/${network}RouteLog.txt`, `["${targetRoute.router1}","${targetRoute.router2}","${targetRoute.token1}","${targetRoute.token2}"],`+"\n", function (err) {});
+      fs.appendFile(`./data/${network}RouteLog.txt`, `["${targetRoute.router1}","${targetRoute.router2}","${targetRoute.token1}","${targetRoute.token2}"],` + "\n", function (err) { });
     }
     if (amtBack.gt(profitTarget)) {
-      await dualTrade(targetRoute.router1,targetRoute.router2,targetRoute.token1,targetRoute.token2,tradeSize);
+      console.log("YAY")
+      await dualTrade(targetRoute.router1, targetRoute.router2, targetRoute.token1, targetRoute.token2, tradeSize);
     } else {
       await lookForDualTrade();
     }
   } catch (e) {
     console.log(e);
-    await lookForDualTrade();	
   }
+  await lookForDualTrade();
 }
 
-const dualTrade = async (router1,router2,baseToken,token2,amount) => {
+const dualTrade = async (router1, router2, baseToken, token2, amount) => {
   if (inTrade === true) {
-    await lookForDualTrade();	
+    await lookForDualTrade();
     return false;
   }
   try {
@@ -89,19 +100,22 @@ const dualTrade = async (router1,router2,baseToken,token2,amount) => {
 }
 
 const setup = async () => {
-  [owner] = await ethers.getSigners();
-  console.log(`Owner: ${owner.address}`);
-  const IArb = await ethers.getContractFactory('Arb');
+  [owner] = await hre.ethers.getSigners();
+  const IArb = await hre.ethers.getContractFactory('Arb');
   arb = await IArb.attach(config.arbContract);
   balances = {};
+  console.log(`Owner: ${owner.address}`);
+  console.log("ARB contract address: ", arb.address)
+
   for (let i = 0; i < config.baseAssets.length; i++) {
     const asset = config.baseAssets[i];
-    const interface = await ethers.getContractFactory('WETH9');
-    const assetToken = await interface.attach(asset.address);
+    const interface = await hre.ethers.getContractFactory('WETH9');
+    const assetToken = interface.attach(asset.address);
     const balance = await assetToken.balanceOf(config.arbContract);
-    console.log(asset.sym, balance.toString());
     balances[asset.address] = { sym: asset.sym, balance, startBalance: balance };
   }
+
+  console.log({ balances })
   setTimeout(() => {
     setInterval(() => {
       logResults();
@@ -112,9 +126,9 @@ const setup = async () => {
 
 const logResults = async () => {
   console.log(`############# LOGS #############`);
-    for (let i = 0; i < config.baseAssets.length; i++) {
+  for (let i = 0; i < config.baseAssets.length; i++) {
     const asset = config.baseAssets[i];
-    const interface = await ethers.getContractFactory('WETH9');
+    const interface = await hre.ethers.getContractFactory('WETH9');
     const assetToken = await interface.attach(asset.address);
     balances[asset.address].balance = await assetToken.balanceOf(config.arbContract);
     const diff = balances[asset.address].balance.sub(balances[asset.address].startBalance);
@@ -123,14 +137,14 @@ const logResults = async () => {
   }
 }
 
-process.on('uncaughtException', function(err) {
+process.on('uncaughtException', function (err) {
   console.log('UnCaught Exception 83: ' + err);
   console.error(err.stack);
-  fs.appendFile('./critical.txt', err.stack, function(){ });
+  fs.appendFile('./critical.txt', err.stack, function () { });
 });
 
 process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: '+p+' - reason: '+reason);
+  console.log('Unhandled Rejection at: ' + p + ' - reason: ' + reason);
 });
 
 main()
